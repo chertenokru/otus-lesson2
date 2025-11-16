12fbb9c73dc5ae1712e36cb008f8f257adf3c871
13th <admin@chertenok.ru>
2025-11-16 21:49:03 +0300

    обновление проекта

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
