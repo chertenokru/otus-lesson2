6d298589cd9c3cbe181ade4b7010f5a80f381841
13th <admin@chertenok.ru>
2025-11-17 13:32:40 +0300

    рейтинг вынесен в отдельный компонент

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type {Product} from "@/model/Product.ts";
-import Star from "@/assets/Star.svg"
-import StarFilled from "@/assets/StarFilled.svg"
+import Rating from "@/components/Rating.vue";
 
 defineProps<{ product: Product }>()
 const emits = defineEmits<{ 'category-click': [category: string] }>()
@@ -11,14 +10,9 @@ const emits = defineEmits<{ 'category-click': [category: string] }>()
   <div class="product-card">
     <p class="product-title">{{ product.title }}</p>
     <img :src="product.image" :alt="product.description" width="100px"/>
-    <div class="rating">
-      <StarFilled v-for="n in Math.trunc(product.rating.rate)" :key="n" alt="Rating"
-                  class="star"/>
-      <Star alt="Rating" class="star_sm" v-for="n in 5-Math.trunc(product.rating.rate)" :key="n"/>
-      <p class="rating-text">{{ product.rating.rate }} {{ product.rating.count }} отзывов </p>
-
-    </div>
-    <p class="underline" @click="emits('category-click',product.category)">Категория: {{ product.category }}</p>
+    <Rating :rate="product.rating.rate" :count="product.rating.count"/>
+    <p class="underline" @click="emits('category-click',product.category)">Категория:
+      {{ product.category }}</p>
     <p>Описание: {{ product.description }}</p>
     <p>Цена: {{ product.price }}</p>
   </div>
@@ -36,28 +30,9 @@ const emits = defineEmits<{ 'category-click': [category: string] }>()
   font-weight: bold;
 }
 
-.rating {
-  display: flex;
-  align-items: center;
-}
-
-.star {
-  width: 20px;
-  color: gold;
-}
-
-.star_sm {
-  height: 18px;
-  color: gold;
-  position: relative;
-  top: -1px;
-}
-.underline{
+.underline {
   text-decoration: underline;
   cursor: pointer;
 }
 
-.rating-text {
-  margin-left: 5px;
-}
 </style>
---
+++ src/components/Rating.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import StarFilled from "@/assets/StarFilled.svg"
+import Star from "@/assets/Star.svg"
+
+defineProps<{
+  rate: number
+  count: number
+}>()
+
+</script>
+
+
+<template>
+  <div class="rating">
+    <StarFilled v-for="n in Math.trunc(rate)" :key="n" alt="Rating"
+                class="star"/>
+    <Star alt="Rating" class="star_sm" v-for="n in 5-Math.trunc(rate)" :key="n"/>
+    <p class="rating-text">{{ rate }} {{ count }} отзывов </p>
+
+  </div>
+</template>
+<style scoped>
+
+.rating {
+  display: flex;
+  align-items: center;
+}
+
+.star {
+  width: 20px;
+  color: gold;
+}
+
+.star_sm {
+  height: 18px;
+  color: gold;
+  position: relative;
+  top: -1px;
+}
+
+.rating-text {
+  margin-left: 5px;
+}
+</style>
