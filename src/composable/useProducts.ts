e732eb5ae0a4bcd03c1236ce991aa137c4faef51
chertenokru <admin@chertenok.ru>
2025-11-23 14:03:15 +0300

    замена fetch на axios

--- src/composable/useProducts.ts
+++ src/composable/useProducts.ts
@@ -1,9 +1,9 @@
 import {ref} from "vue";
 import type {Product} from "@/model/Product.ts";
+import api from "@/api.ts";
 
 export const useProducts = () => {
 
-  const url = "https://fakestoreapi.com/products"
   const isLoading = ref<boolean>(false);
   const isError = ref<boolean>(false);
   const productList = ref<Product[]>([])
@@ -14,21 +14,13 @@ export const useProducts = () => {
     isLoading.value = true;
     errorText.value = ''
     try {
-      await loadProducts()
+      productList.value = (await api.get<Product[]>('products')).data;
     } catch (e) {
       errorText.value = `Ошибка загрузки : ${e}`
       console.log(errorText)
       isError.value = true;
     }
     isLoading.value = false
-  }
-
-  const loadProducts = async () => {
-    const result = await fetch(url);
-    if (!result.ok) {
-      throw new Error(result.statusText);
-    }
-    productList.value = await result.json()
   }
 
   return {productList, isLoading, isError, updateProducts, errorText}
